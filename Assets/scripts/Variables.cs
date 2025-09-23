884ad535f8876b9c89a4e1fce944117b48068a6a
wooginski <1036317@student.sae.edu.au>
2025-09-23 19:18:59 +1000

    commit

--- Assets/scripts/Variables.cs
+++ Assets/scripts/Variables.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-
+    #region code neatness tutorial
     //anything after the slash is ignored by unity
 
     /*this is a multi line comment 
@@ -21,7 +21,14 @@ public class Variables : MonoBehaviour
     public bool mySecondBool =false
     [Space(20)] // a space creates a space in the inspector based on the number put in
     public bool myThirdBool = false;
+    #endregion
 
+    public int myFirstInt = 1 // an int can only hold whole numbers
+    private float myHeight = 180.67f; // a float holds decimal numbers
+    public double myFavNumber = 187.45; // a double holds double a float and requires double memory
+    public string myName = "Noah" // a string can only hold words 
+    public char myInitial = 'S' // a char can only hold a single character
+    public bool isWearingAWatch = false; // a bool can only hold true or false
 
     #region all of my unity functions
     /// <summary>
