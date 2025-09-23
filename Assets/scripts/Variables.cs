c015f6847587415b0c51734117850dbc7835c5dd
wooginski <1036317@student.sae.edu.au>
2025-09-23 18:52:33 +1000

    initial variables

--- Assets/scripts/Variables.cs
+++ Assets/scripts/Variables.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/// <summary>
+/// this is the summary which holds all the information of my variables activity
+/// </summary>
 
 public class Variables : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //anything after the slash is ignored by unity
+
+    /*this is a multi line comment 
+     * i can keep writing
+     * until i put in the
+     */
+
+    [Tooltip("this bool holds true or false")] // a tooltip provides a message when we hover over a variable in our inspector
+    public bool myFirstBool = true;
+    [header("Movement related Variables")] // a header creates a heading in our inspector for our variables
+    public bool mySecondBool =false
+    [Space(20)] // a space creates a space in the inspector based on the number put in
+    public bool myThirdBool = false;
+
+
+    #region all of my unity functions
+    /// <summary>
+    /// the start is called on the first frame
+    /// </summary>
     void Start()
     {
         
@@ -15,4 +37,6 @@ public class Variables : MonoBehaviour
     {
         
     }
+
+    #endregion
 }
