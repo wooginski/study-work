cb44f7f3fe1757c3d269c0e03c7a342c25875656
wooginski <1036317@student.sae.edu.au>
2025-09-23 20:09:57 +1000

    debugg

--- Assets/scripts/Debugging.cs
+++ Assets/scripts/Debugging.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Debugging : MonoBehaviour
 {
+
+    public string myName = "Noah";
+    public int myAge = 23;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
+        Debug.Log("My current age is "+myAge);
+        myAge = myAge + 10;
+        Debug.Log("My new age is " + myAge + "how did I age so fast");
+
+        Debug.LogWarning("This is bad");
+        Debug.LogError("this will break things");
     }
 
     // Update is called once per frame
